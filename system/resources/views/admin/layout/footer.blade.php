267447f595ebf23ffeb6a4276452d6fb005c56a6
Ade Maulana Alfaqi <107929610+ademaulanaalfaqi@users.noreply.github.com>
2024-02-02 15:08:49 +0700

    first

---
+++ system/resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+      <div class="mb-2 mb-md-0">
+        ©
+        <script>
+            document.write(new Date().getFullYear());
+        </script>        
+        <a href="https://en.wikipedia.org/wiki/Internet_of_things" target="_blank" class="footer-link fw-bolder">Internet of Things</a>
+      </div>
+    </div>
+  </footer>
